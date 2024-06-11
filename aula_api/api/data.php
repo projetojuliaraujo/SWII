676f4ff56cd608c860526712ce2c2caf44b25e50
Júlia Araújo Nogueira de Freitas <100208717+projetojuliaraujo@users.noreply.github.com>
2024-06-11 08:19:02 -0300

    Add files via upload

---
+++ aula_api/api/data.php
@@ -0,0 +1,45 @@
+<?php
+
+return [
+    [
+        'nome' => 'Anderson',
+        'email' => 'anderson@gmail.com'
+    ],
+    [
+        'nome' => 'Júlia',
+        'email' => 'julia@gmail.com'
+    ],
+    [
+        'nome' => 'Marcolas Peralta',
+        'email' => 'marcolasperalta@gmail.com'
+    ],
+    [
+        'nome' => 'Jungkook',
+        'email' => 'jungkook@gmail.com'
+    ],
+    [
+        'nome' => 'Jimin',
+        'email' => 'jimin@gmail.com'
+    ],
+    [
+        'nome' => 'Taehyung',
+        'email' => 'taehyung@gmail.com'
+    ],
+    [
+        'nome' => 'Jung Hoseok',
+        'email' => 'hoseok@gmail.com'
+    ],
+    [
+        'nome' => 'Namjoon',
+        'email' => 'namjoon@gmail.com'
+    ],
+    [
+        'nome' => 'Yoongi',
+        'email' => 'yoongi@gmail.com'
+    ],
+    [
+        'nome' => 'Seokjin',
+        'email' => 'seokjin@gmail.com'
+    ],
+        
+];
